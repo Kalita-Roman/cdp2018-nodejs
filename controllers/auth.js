d55fe116f1ff90ebd5b161c3b8babeaf6be3c554
Kalita-Roman <raman_kalita@mail.ru>
2018-06-25 12:41:09 +0300

    hw5: task 2. Add JWT.

---
+++ controllers/auth.js
@@ -0,0 +1,3 @@
+export function getUser() {
+    //TODO: implement it
+}
--- middlewares/routes/auth.js
+++ middlewares/routes/auth.js
@@ -1,5 +1,28 @@
-export const auth = (req, res) => {
-    res
+import { getUser } from 'controllers/auth';
+import { generateJWT } from 'services/jwt';
+
+const SECRET = 'secret';
+
+export const auth = async (req, res) => {
+    const { body } = req;
+    const user = await getUser(body);
+    if (!user) {
+        return res
+            .status(404)
+            .json({
+                code: 404,
+                message: 'Not Found',
+                data: { }
+            });
+    }
+    const token = generateJWT({ user }, SECRET);
+    return res
+        .cookie('token', token)
         .status(200)
-        .json({});
+        .json({ 
+            code: 200,
+            message: 'OK',
+            data: { user },
+            token
+        });
 };
