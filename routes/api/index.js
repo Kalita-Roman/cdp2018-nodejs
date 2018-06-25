02efa2a4d3ca9db13369b49b54dc476b48071ca4
Roman Kalita <raman_kalita@mail.ru>
2018-06-25 21:33:19 +0300

    hw5: task 3. Add middleware to verify JWT.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import users from './users';
 import products from './products';
+import verification from '../../middlewares/verification';
 
 const router = new Router();
+router.use(verification);
 router.use('/users', users);
 router.use('/products', products);
 
