b3216860c787d525be75969345122b89aa2a49ca
Roman Kalita <raman_kalita@mail.ru>
2018-07-12 23:29:35 +0300

    hw5: tasks 4,5,6. Implement a local authentication strategy. Implement an authentication via the providers.

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,5 +1,13 @@
 export const logUser = (req, res, next) => {
-    const { id, type } = req.user;
-    console.log('user:', { id, type });
+    const {user} = req;
+    if(user) {
+        const { id, type } = user;
+        console.log('user:', { id, type });
+    };
+    next();
+};
+
+export const createLogMiddleware = (text) => (req, res, next) => {
+    console.log(text);
     next();
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,36 @@
 import { Router } from 'express';
 import passport from 'passport';
 import { auth } from 'middlewares/routes/auth';
+import { createLogMiddleware } from 'middlewares/logger';
 
 const router = new Router();
-router.get('/logout',   (req, res) => {
-    console.log('logout //TODO: Implement it!');
-    res.sendStatus(200);
+
+const createProviderAuthRoute = (providerName, authOptions) => {
+    const providerRouter = new Router();
+    providerRouter.get('/',
+        createLogMiddleware(`auth/${providerName}`),
+        passport.authenticate(providerName, authOptions),
+    );
+    providerRouter.get('/callback',
+        createLogMiddleware(`auth/${providerName}/callback`),
+        passport.authenticate(providerName,  {
+            successRedirect: '/',
+            failureRedirect: '/login',
+        }),
+        (req, res) => res.redirect('/')
+    );
+    return providerRouter;
+};
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 router.post('/local', passport.authenticate('local'), auth);
-router.get('/facebook',
-    (req, res, next) => {
-        console.log('auth/facebook');
-        next();
-    }, 
-    passport.authenticate('facebook'),
-);
-router.get('/facebook/callback', 
-    (req, res, next) => {
-        console.log('auth/facebook/callback');
-        next();
-    },
-    passport.authenticate('facebook', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }),
-    function(req, res) {
-        res.redirect('/');
-    }
-);
+router.use('/facebook', createProviderAuthRoute('facebook'));
+router.use('/twitter', createProviderAuthRoute('twitter'));
+router.use('/google', createProviderAuthRoute('google', 
+    { scope: ['https://www.googleapis.com/auth/plus.login'] }
+));
 
 export default router;
