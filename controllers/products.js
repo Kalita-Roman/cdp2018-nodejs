730852da7e082a635a8ebf49d16b891f14734842
Roman Kalita <raman_kalita@mail.ru>
2018-07-22 14:38:52 +0300

    hw6: GET, POST Products

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,17 @@
-import { readFilePromise, writeFilePromise } from 'utils/fsUtils';
+import { readFilePromise } from 'utils/fsUtils';
+import Product from 'models/Product';
 
-const PRODUCTS_FILE = './data/api/products.json';
 const REVIEWS_FILE = './data/api/reviews.json';
 
 export const fetchProducts = () => {
-    return readFilePromise(PRODUCTS_FILE);
+    return Product.findAll({ raw : true });
 };
 
-export const fetchProductById = async idParam => {
-    const products = await readFilePromise(PRODUCTS_FILE);
-    return products.find(({ id }) => id === idParam);
+export const fetchProductById = idParam => {
+    return Product.find({ 
+        where: { id: idParam }, 
+        raw : true 
+    });
 };
 
 export const fetchReviewsById = async idParam => {
@@ -18,16 +20,7 @@ export const fetchReviewsById = async idParam => {
 };
 
 export const addProduct = async newProduct => {
-    const products = await readFilePromise(PRODUCTS_FILE);
-    const newProducts = pushProduct(products, newProduct);
-    await writeFilePromise(PRODUCTS_FILE, newProducts);
-    return newProduct;
+    const { dataValues } = await Product.create(newProduct);
+    return dataValues;
 };
 
-function pushProduct(target, product) {
-    if (!Array.isArray(target)) {
-        return [product];
-    } 
-    target.push(product);
-    return target;
-}
--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,15 @@
-export default class Product {
-    constructor() {
-        console.log('Product module');
-    }
-}
+import Sequelize from 'sequelize';
+import db from 'services/sequelizeService';
+
+const Product = db.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: Sequelize.STRING,
+});
+
+Product.sync();
+
+export default Product;
