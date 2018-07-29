f11910d2e578331b393fac36b353a8e347dbb11d
Roman Kalita <raman_kalita@mail.ru>
2018-07-29 20:55:41 +0300

    hw7: Add the routes to POST, PUT, DELETE cities

--- middlewares/routes/cities.js
+++ middlewares/routes/cities.js
@@ -1,5 +1,8 @@
 import {
     fetchCities,
+    addCity,
+    removeCityById,
+    updateCityById
 } from 'controllers/cities';
 
 export const getCities = async (req, res) => {
@@ -7,3 +10,19 @@ export const getCities = async (req, res) => {
     res.status(200).json(cities);
 };
 
+export const postCities = async (req, res) => {
+    const city = await addCity(req.body);
+    res.status(200).json(city);
+};
+
+export const putCityById = async (req, res) => {
+    const { body, params: { id }} = req;
+    const city = await updateCityById(id, body);
+    res.status(200).json(city);
+};
+
+export const deleteCityById = async (req, res) => {
+    const { id } = req.params;
+    const deletedItemsCount = await removeCityById(id);
+    return res.status(200).json({ deletedItemsCount });
+};
