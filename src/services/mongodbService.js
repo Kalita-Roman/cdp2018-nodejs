02bc35dde17d87bed8ea84ea00645f03acc85b06
Roman Kalita <raman_kalita@mail.ru>
2018-07-29 13:32:14 +0300

    improvements: Update configs. Split to the separate files

--- src/services/mongodbService.js
+++ src/services/mongodbService.js
@@ -1,13 +1,11 @@
 import { MongoClient } from 'mongodb';
 
-const username = 'nodejs-cdp';
-const password = 'nodejs-2018';
-const url = `mongodb://${username}:${password}@ds247141.mlab.com:47141/nodejs-cdp`;
+import { mongoDBconnectionUrl } from 'config';
 
 const dbName = 'nodejs-cdp';
 
 const db = (method) => {
-    MongoClient.connect(url, function (error, client) {
+    MongoClient.connect(mongoDBconnectionUrl, { useNewUrlParser: true }, function (error, client) {
         if(error) {
             return console.error('Connection to mongodb is failed!');
         }
