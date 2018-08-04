032028f909c47a8687995d1dfb03e6d8df545738
Roman Kalita <raman_kalita@mail.ru>
2018-08-04 12:50:25 +0300

    hw7: Add 'lastModifiedPlugin'

--- src/services/mongooseServise.js
+++ src/services/mongooseServise.js
@@ -1,10 +1,17 @@
 import mongoose from 'mongoose';
 import { mongoDBconnectionUrl } from 'config';
+import lastModifiedPlugin from './lastModifiedPlugin';
 
-const Schema = mongoose.Schema;
+mongoose.plugin(lastModifiedPlugin);
+
+const { Schema } = mongoose;
 
 const connection = mongoose.createConnection(mongoDBconnectionUrl, { useNewUrlParser: true });
 
 export default connection;
 
-export const createModel = (modelName, schema) => connection.model(modelName, new Schema(schema));
+export const createModel = (modelName, schemaPattern) => {
+    const schema = new Schema(schemaPattern);
+    schema.plugin(lastModifiedPlugin);
+    return connection.model(modelName, schema);
+};
